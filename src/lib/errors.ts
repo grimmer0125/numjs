baddc3222fcb318815b622ed9e5ad19834aecac3
grimmer <grimmer0125@gmail.com>
2021-10-25 23:03:40 +0800

    Remove the commented code in errors.ts

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,25 +1,5 @@
 "use strict";
 
 export class ValueError extends Error {}
-
-// function ValueError() {
-//   const err = Error.apply(this, arguments);
-//   err.name = this.constructor.name;
-//   return err;
-// }
-
 export class ConfigError extends Error {}
-
-// function ConfigError() {
-//   const err = Error.apply(this, arguments);
-//   err.name = this.constructor.name;
-//   return err;
-// }
-
 export class NotImplementedError extends Error {}
-
-// function NotImplementedError() {
-//   const err = Error.apply(this, arguments);
-//   err.name = this.constructor.name;
-//   return err;
-// }
